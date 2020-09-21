6899e79949a35a615fa6c6c2a9c68c3ca8050564
TiagoSRangel <71654008+TiagoSRangel@users.noreply.github.com>
2020-09-21 17:06:22 +0100

    Exercício 2

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] chave5numeros = new int[5];
+
+            Random numerosrandom = new Random();
+
+            for (int x = 0; x < 5; x++)
+            {
+                while (true)
+                {
+                    int sorteio = numerosrandom.Next(1, 51);
+                }
+            }
+         
+        }
+    }
+}
